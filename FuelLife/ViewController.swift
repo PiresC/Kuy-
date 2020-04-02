7cd554a4dc009f7a0edaf35b00d558f9447e7813
Amos Ebenhaezer <amosehaezer@gmail.com>
2020-04-02 18:22:33 +0700

    Initial Commit

---
+++ FuelLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FuelLife
+//
+//  Created by Amos Ebenhaezer on 02/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
