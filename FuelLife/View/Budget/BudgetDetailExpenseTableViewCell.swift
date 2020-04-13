36a396c06647a96fdc06858850d88aa32c7bed9e
Edwin Sendjaja <edwin.sendjaja@gmail.com>
2020-04-13 23:01:53 +0700

    add expense layout in budget detail page

--- FuelLife/View/Budget/BudgetDetailExpenseTableViewCell.swift
+++ FuelLife/View/Budget/BudgetDetailExpenseTableViewCell.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class BudgetDetailExpenseTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var expenseDateLabel: UILabel!
+    @IBOutlet weak var expensePriceLabel: UILabel!
+    
+    
+    var expense: Expense!{
+        didSet{
+            update()
+        }
+    }
+    
+    var expenseBackgroundColor: String = ""
+            
+    func update(){
+        self.contentView.backgroundColor = UIColor(hexString: expenseBackgroundColor)
+
+        expensePriceLabel.text = "\(expense.price)"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d yyyy"
+        let dateString = dateFormatter.string(from: expense.date!)
+        
+        expenseDateLabel.text = dateString
+
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,7 +43,7 @@ class BudgetDetailExpenseTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
+
--- FuelLife/View/Budget/BudgetDetailTableViewCell.swift
+++ FuelLife/View/Budget/BudgetDetailTableViewCell.swift
@@ -10,48 +10,67 @@ import UIKit
 
 class BudgetDetailTableViewCell: UITableViewCell {
     
-    
     @IBOutlet weak var budgetDetailEntertainmentLabel: UILabel!
     
     @IBOutlet weak var budgetDetailEntertainmentPercentage: UILabel!
-    
-    @IBOutlet weak var budgetDetailCellView: UIView!
-    
+
+    @IBOutlet weak var budgetDetailExpenseTableView: UITableView!
+        
     var budgetDetail:BudgetDetail!{
         didSet{
             update()
         }
     }
-    
-    func update(){
         
+    func update(){
+
         budgetDetailEntertainmentLabel.text = budgetDetail.name
         budgetDetailEntertainmentPercentage.text = "\(budgetDetail.percentage)"
             
         self.contentView.backgroundColor = UIColor.clear
-        let whiteRoundedView : UIView = UIView(frame: CGRect(x: 10, y: 8, width: self.frame.size.width - 20, height: 120))
+        let whiteRoundedView : UIView = UIView(frame: CGRect(x: 10, y: 8, width: self.frame.size.width - 20, height: 180))
 
         whiteRoundedView.layer.backgroundColor = UIColor(hexString: budgetDetail.backgroundColor).cgColor
-         whiteRoundedView.layer.masksToBounds = false
+        whiteRoundedView.layer.masksToBounds = false
         whiteRoundedView.layer.cornerRadius = 10.0
         whiteRoundedView.layer.shadowOffset = CGSize(width: -1, height: 1)
-         whiteRoundedView.layer.shadowOpacity = 0.2
+        whiteRoundedView.layer.shadowOpacity = 0.2
 
-         self.contentView.addSubview(whiteRoundedView)
-         self.contentView.sendSubviewToBack(whiteRoundedView)
-        
-        
+        self.contentView.addSubview(whiteRoundedView)
+        self.contentView.sendSubviewToBack(whiteRoundedView)
+
     }
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        budgetDetailExpenseTableView.delegate = self
+        budgetDetailExpenseTableView.dataSource = self
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+}
 
-        // Configure the view for the selected state
+extension BudgetDetailTableViewCell: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return budgetDetail?.expenses.count ?? 0
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "budgetDetailExpenseTableViewCell", for: indexPath) as! BudgetDetailExpenseTableViewCell
+        
+        if budgetDetail?.expenses.count ?? 0 > 0 {
+            cell.expenseBackgroundColor = budgetDetail.backgroundColor
+            cell.expense = budgetDetail.expenses[indexPath.row]
+        }
+       
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+    
+    
 }
